02a244e4699cfb13cb966101a5a43ea91d208352
memento999 <janokakashvili@gmail.com>
2020-05-17 02:48:34 +0400

    fp

---
+++ overlay.js
@@ -0,0 +1,30 @@
+// open overlay
+document.addEventListener("click", function () {
+  document.getElementById("overlayNavIcon").onclick = openOverlay;
+  // close overlay on another click
+  if (document.getElementById("overlay").style.width == "78%") {
+    document.getElementById("burger-menu").onclick = closeOverlay;
+  }
+});
+
+// Open Overlay func
+function openOverlay() {
+  // set width to 100% to show up overlay
+  document.getElementById("overlay").style.width = "78%";
+  // disable scrolling
+  document.querySelector("body").style.overflow = "hidden";
+  // change image on click
+  document.getElementById("overlayNavIcon").style.display = "none";
+  document.getElementById("close-icon").style.display = "block";
+}
+
+// Close Overlay func
+function closeOverlay() {
+  // set width to 0px to hide overlay
+  document.getElementById("overlay").style.width = "0px";
+  // enable scrolling
+  document.querySelector("body").style.overflow = "visible";
+  // change image on click
+  document.getElementById("close-icon").style.display = "none";
+  document.getElementById("overlayNavIcon").style.display = "block";
+}
